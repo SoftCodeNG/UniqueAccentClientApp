9076fe01004aa642a017e1d4dcd8072636dfef52
vickysomtee <vickysomtee@gmail.com>
2021-04-30 00:15:16 -0700

    Added five new screens to the courses section

---
+++ src/app/pages/courses/course-grid-list/course-grid-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-course-grid-list',
+  templateUrl: './course-grid-list.component.html',
+  styleUrls: ['./course-grid-list.component.scss']
+})
+export class CourseGridListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
