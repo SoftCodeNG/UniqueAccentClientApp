282a58a5ef4510c1b3460c7e5f513ccd460ebfbb
Uche Esther <ucheesther87@gmail.com>
2021-05-07 18:24:03 +0100

    integration

--- src/app/pages/courses/course-section/course-section.component.ts
+++ src/app/pages/courses/course-section/course-section.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {CoursesService} from '../../../core/services/courses.service';
 
 @Component({
   selector: 'app-course-section',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./course-section.component.scss']
 })
 export class CourseSectionComponent implements OnInit {
+   public courseDetails: any;
+   public lessonDetails: any;
 
-  constructor() { }
+  constructor(private coursesService: CoursesService) { }
 
   ngOnInit(): void {
+    this.getCourseDetails('accent-polishing-course1619136362.08034');
+    this.getCourseLessons(36);
   }
 
+  getCourseDetails(slug: string): void {
+    this.coursesService.getCourseDetails(slug).subscribe(res => {
+      this.courseDetails = res;
+    });
+  }
+
+  getCourseLessons(id: number): void {
+    this.coursesService.getCourseLessons(id).subscribe(res => {
+      this.lessonDetails = res;
+    });
+  }
 }
