4cd738a82a8a137883ec2072825b049df676947c
Augustine Nwafor <austineforall@gmail.com>
2021-05-14 18:24:48 +0100

    Worked on Authentication

--- src/app/pages/courses/courses-routing.module.ts
+++ src/app/pages/courses/courses-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {CourseListComponent} from "./course-list/course-list.component";
-import {AboutCourseComponent} from "./about-course/about-course.component";
+import {CourseListComponent} from './course-list/course-list.component';
+import {AboutCourseComponent} from './about-course/about-course.component';
 
 const routes: Routes = [
   {
