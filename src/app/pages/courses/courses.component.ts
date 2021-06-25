1a65b52b0e64d4ea085204f0c8a5ec4c7bb90b1c
Augustine Nwafor <austineforall@gmil.com>
2021-06-25 23:50:50 +0100

    Worked on User Profile

--- src/app/pages/courses/courses.component.ts
+++ src/app/pages/courses/courses.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {CourseService} from "../../core/services/course.service";
+import {Select, Store} from "@ngxs/store";
+import {SetCourseList} from "../../store/app-store/app.action";
+import {Observable} from "rxjs";
+import {AppState} from "../../store/app-store/app.state";
 
 @Component({
   selector: 'app-courses',
@@ -10,19 +14,23 @@ export class CoursesComponent implements OnInit {
   currentView = 'grid';
   allCourses: any[];
 
+  @Select(AppState.getCourseList) courseList$: Observable<any>;
   constructor(
     private coursesService: CourseService,
-  ) {
-  }
+    private store: Store
+  ) {}
 
   ngOnInit(): void {
+    this.courseList$.subscribe(res => {
+      this.allCourses = res;
+    });
     this.getAllCourses();
   }
 
   getAllCourses(): void {
     this.coursesService.getAllCourses().subscribe(res => {
       console.log(res);
-      this.allCourses = res;
+      this.store.dispatch(new SetCourseList(res));
     });
   }
 
@@ -30,7 +38,7 @@ export class CoursesComponent implements OnInit {
     if (value) {
       this.coursesService.searchAllCourses(value).subscribe(res => {
         console.log(res);
-        this.allCourses = res;
+        this.store.dispatch(new SetCourseList(res));
       });
     } else {
       this.getAllCourses();
