1a0371e81d180a36278b61ae2db3cc86f8b25e43
Adeola <adeolaadefuye@gmail.com>
2021-06-30 19:50:57 +0100

    Updates

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -3,16 +3,25 @@ import {Store} from '@ngxs/store';
 import {SetHeaderVisibility} from '../../store/app-store/app.action'; // lib
 
 
+// @ts-ignore
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent implements OnInit {
+  private courseService: any;
+  private courseDetails: any;
 
-  constructor(private store: Store) { }
+  constructor(private store: Store) {
+  }
 
   ngOnInit(): void {
     this.store.dispatch(new SetHeaderVisibility('visible'));
   }
+  // getCoursesDetails(slug: string): void{
+  //   this.courseService.getCourseDteail(slug).subscribe(res => {
+  //   this.courseDetails = res; };
+  // })
+
 }
